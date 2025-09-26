7ff681200204d2bc8b012197e34604b89b58803b
mtlouzada <matheuslouzadaa@gmail.com>
2025-09-25 21:43:48 -0300

    refactor: refatoando a lógica atual para separar o db em uma camada e alinhar a logica do objeto para efitar erro 500

--- src/Desafio.Umbler/Models/DatabaseContext.cs
+++ src/Desafio.Umbler/Models/DatabaseContext.cs
@@ -4,26 +4,26 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Desafio.Umbler.Models
 {
-    public class DatabaseContext : DbContext
-    {
-        public DatabaseContext(DbContextOptions<DatabaseContext> options)
-        : base(options)
-        {
-
-        }
-
-        public DbSet<Domain> Domains { get; set; }
-    }
-
     public class Domain
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Ip { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public string WhoIs { get; set; }
-        public int Ttl { get; set; }
-        public string HostedAt { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Ip { get; set; } = "0.0.0.0";
+        
+        [Required]
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        [Required]
+        public string WhoIs { get; set; } = string.Empty;
+
+        [Required]
+        public int Ttl { get; set; } = 60;
+
+        public string HostedAt { get; set; } = "Desconhecido";
     }
 }
